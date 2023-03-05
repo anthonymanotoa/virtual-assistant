49e7194ac18b11bc84c2c6892e55b1e4083ec45c
anthonymanotoa <aimm1999@gmail.com>
2023-03-05 16:33:09 -0500

    Now the assistant understands the instructions

--- web.py
+++ web.py
@@ -4,22 +4,32 @@ import os
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
+messages = [
+    {"role": "system", "content": "You are a helpful assistant."}
+]
+
 
 def transcribe(audio):
+    global messages
     audio_file = open(audio, "rb")
     transcript = openai.Audio.transcribe("whisper-1", audio_file)
-    print(transcript)
+
+    messages.append({"role": "user", "content": transcript["text"]})
 
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
-        messages=[
-            {"role": "system", "content": "You are a helpful assistant."},
-            {"role": "user", "content": transcript["text"]}
-        ]
+        messages=messages
     )
-    print(response)
 
-    return transcript["text"]
+    system_message = response["choices"][0]["message"]["content"]
+    messages.append({"role": "assistant", "content": system_message})
+
+    chat_transcript = ""
+    for message in messages:
+        if message["role"] != "system":
+            chat_transcript += message["role"] + ": " + message["content"] + "\n\n"
+
+    return chat_transcript
 
 
 ui = gr.Interface(fn=transcribe, inputs=gr.Audio(source="microphone", type="filepath"), outputs="text")
