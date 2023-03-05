c07ee1c94167d4d3a1335ee1d52b9158bb44d45b
anthonymanotoa <aimm1999@gmail.com>
2023-03-05 16:24:07 -0500

    Audio transcribe and response

--- web.py
+++ web.py
@@ -1,9 +1,25 @@
 import gradio as gr
+import openai
+import os
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
 
 def transcribe(audio):
-    print(audio)
-    return "Transcription goes here"
+    audio_file = open(audio, "rb")
+    transcript = openai.Audio.transcribe("whisper-1", audio_file)
+    print(transcript)
+
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": transcript["text"]}
+        ]
+    )
+    print(response)
+
+    return transcript["text"]
 
 
 ui = gr.Interface(fn=transcribe, inputs=gr.Audio(source="microphone", type="filepath"), outputs="text")
